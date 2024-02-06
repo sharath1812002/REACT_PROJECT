171a1cf8e6f3ebde09b7881a0c08e18733b915dd
sharath <sharath1812002@gmail.com>
2024-02-06 23:18:00 +0530

    Created footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./component/Header";
+import Footer from "./component/Footer";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
     
     <Header/>
+    <Container>
+      <main>
+
+      </main>
+    </Container>
+    <Footer/>
     </>
   );
 }
